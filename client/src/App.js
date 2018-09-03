70c5f92e0b0fae8d705c91cad6b2d36776e1be20
Logan Clements <loganclements44977@gmail.com>
2018-09-02 22:49:48 -0400

    added the planet spinner and added swipe package to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import Swipe from 'react-easy-swipe';
+
 import Header from './Header';
 import PlanetText from './PlanetText';
 import Astronaut from './Astronaut';
+import PlanetSlider from './PlanetSlider';
 
 class App extends Component {
+
+  swipeRight(){
+    console.log('swipe right');
+  }
+
+  swipeLeft(){
+    console.log('swipe left');
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <PlanetText/>
-        <Astronaut/>
+      <div className="app">
+        <Swipe
+          className="app-swipe"
+          onSwipeRight={this.swipeRight}
+          onSwipeLeft={this.swipeLeft}>
+
+          <Header/>
+          <PlanetText/>
+          <Astronaut/>
+          <PlanetSlider/>
+
+        </Swipe>
       </div>
     );
   }
--- client/src/PlanetSlider.js
+++ client/src/PlanetSlider.js
@@ -4,7 +4,21 @@ import './PlanetSlider.css';
 class PlanetSlider extends Component{
    render(){
        return(
-           <div></div>
+           <div className="planetslider planetslider-appear">
+                <div className="planetslider-circle"></div>
+                <div className="planetslider-planet-earth">
+                    <div className="planetslider-planet-earth-moon moon-orbit-earth"/>
+                </div>
+
+                <div className="planetslider-holder-text">
+                    <p className="planetslider-text-planet">EARTH</p>
+                </div>
+
+                <div className="planetslider-holder-arrows">
+                    <img className="planetslider-arrow-right" alt="" src="/img/planetslider/arrow1.svg"/>
+                    <img className="planetslider-arrow-left" alt="" src="/img/planetslider/arrow2.svg"/>
+                </div>
+           </div>
        );
    }
 }
